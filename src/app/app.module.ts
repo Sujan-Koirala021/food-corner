301e3949dfb258f58426becfe12ab9260f5b2d99
Sujan Koirala <077bct087.sujan@pcamupus.edu.np>
2023-06-24 20:17:49 +0545

    Add home component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './MyComponents/header/header.component';
+import { HomeComponent } from './MyComponents/home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
