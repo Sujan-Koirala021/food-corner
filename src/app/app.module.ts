e14bdf32cbd295f691c8927e7dc8514670790f35
Sujan Koirala <077bct087.sujan@pcamupus.edu.np>
2023-06-26 22:53:47 +0545

    Add search component and added searching functionality

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,16 +5,20 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './MyComponents/header/header.component';
 import { HomeComponent } from './MyComponents/home/home.component';
+import { SearchComponent } from './MyComponents/search/search.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    HomeComponent
+    HomeComponent,
+    SearchComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
